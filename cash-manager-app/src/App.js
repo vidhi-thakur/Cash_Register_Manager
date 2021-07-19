0c04098e9cb527a0fd157965f3efd29fb70446ec
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-19 16:06:09 +0530

    initial commit

---
+++ cash-manager-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h2 className="app__heading">Cash Register Manager</h2>
+      <p>Enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
+    </div>
+  );
+}
+
+export default App;
